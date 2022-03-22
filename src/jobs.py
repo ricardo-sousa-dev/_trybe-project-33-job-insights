a0b55a39b0c88de53f4a7fca4852ac35f6078268
ricardo-sousa <rwmsousa@gmail.com>
2022-03-22 15:27:40 -0300

    feat: requirement 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,27 @@
+# -*- coding: utf-8 -*-
+import csv
 from functools import lru_cache
+
+# 1 - Implemente a função read
+
+# Para começarmos a processar os dados, devemos antes carregá-los
+# em nossa aplicação.
+# Esta função será responsável por abrir o arquivo CSV e
+# retornar os dados no formato de uma lista de dicionários.
+
+# A função deve receber um path (uma string com o caminho para um arquivo).
+# A função deve abrir o arquivo e ler seus conteúdos.
+# A função deve tratar o arquivo como CSV.
+# A função deve retornar uma lista de dicionários, onde as chaves
+# são os cabeçalhos de cada coluna e os valores correspondem a cada linha.
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    data = []
+    with open(path) as file:
+        jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        for row in jobs_reader:
+            data.append(row)
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    return data
